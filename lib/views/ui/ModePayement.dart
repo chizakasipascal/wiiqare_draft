3cfb62bab3c4974dd7f06ef579f8a2d92fb18487
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-01-20 04:54:17 -0800

    qcode scanner ok

--- lib/views/ui/ModePayement.dart
+++ lib/views/ui/ModePayement.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:wiiqare/constants/routes.dart';
 import 'package:wiiqare/utils/colors.dart';
 import 'package:wiiqare/views/widgets/Background/background.dart';
 import 'package:wiiqare/views/widgets/widgets.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class ModePayement extends StatefulWidget {
   @override
@@ -12,27 +14,97 @@ class _ModePayementState extends State<ModePayement> {
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
-    return Stack(
-      children: [
-        Container(
-          color: White,
-          height: size.height,
-          child: Align(
-            alignment: Alignment.bottomCenter,
-            child: Column(
-              children: [
-                SingleTitle(singleTitle: "Autre methdodes de paiement")
-              ],
+    return Scaffold(
+      body: Column(
+        children: [
+          SizedBox(
+            height: size.height * .7,
+            child: WelcomBackGround(
+              widget: Center(
+                child: Icon(
+                  Icons.qr_code_scanner,
+                  size: size.height * .4,
+                ),
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          height: size.height * .7,
-          child: WelcomBackGround(
-            widget: Center(child: Placeholder()),
+          Container(
+            color: White,
+            height: size.height * .3,
+            child: Align(
+              alignment: Alignment.bottomCenter,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    horizontal: 20.0, vertical: 20.0),
+                child: Column(
+                  children: [
+                    SingleTitle(singleTitle: "Autre methdodes de paiement"),
+                    SizedBox(height: 20),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Material(
+                                color: BlueText.withOpacity(.4),
+                                child: IconButton(
+                                  icon: Icon(
+                                    Icons.phone_android,
+                                    color: Bleu,
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 5),
+                            SingleTitle(singleTitle: "Numero de telephone")
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            ClipRRect(
+                              borderRadius: BorderRadius.circular(10),
+                              child: Material(
+                                color: BlueText.withOpacity(.4),
+                                child: IconButton(
+                                  icon: Icon(
+                                    FontAwesomeIcons.barcode,
+                                    color: Bleu,
+                                  ),
+                                  onPressed: () {},
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 5),
+                            SingleTitle(singleTitle: "Barcode")
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: 10),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(50),
+                      child: Material(
+                        color: Yello,
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.qr_code_scanner,
+                            color: Bleu,
+                          ),
+                          onPressed: () => Navigator.pushNamed(
+                              context, Routes.qRCodeScanner),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
