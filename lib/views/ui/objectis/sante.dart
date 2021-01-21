06b99aad03d5a8c3257190017ef79ce3d0ff6bdf
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-01-21 08:32:00 -0800

    combo ok

--- lib/views/ui/objectis/sante.dart
+++ lib/views/ui/objectis/sante.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
-
+import 'package:wiiqare/utils/colors.dart';
+import '../../../models/comboxSante.dart';
 import 'package:wiiqare/views/widgets/Background/background.dart';
 import 'package:flutter_rounded_progress_bar/flutter_rounded_progress_bar.dart';
 import 'package:flutter_rounded_progress_bar/rounded_progress_bar_style.dart';
+import 'package:wiiqare/views/widgets/objectifWidgets/montantSetp.dart';
 
 import 'package:wiiqare/views/widgets/widgets.dart';
 
@@ -12,6 +14,36 @@ class Sante extends StatefulWidget {
 }
 
 class _SanteState extends State<Sante> {
+  List<ComboSante> _santesss = ComboSante.getComboSante();
+  List<DropdownMenuItem<ComboSante>> _dropdownMenuItems;
+  ComboSante _selectedSanteDure;
+
+  @override
+  void initState() {
+    _dropdownMenuItems = buildDropdownMenuItems(_santesss);
+    _selectedSanteDure = _dropdownMenuItems[0].value;
+    super.initState();
+  }
+
+  List<DropdownMenuItem<ComboSante>> buildDropdownMenuItems(List santes) {
+    List<DropdownMenuItem<ComboSante>> items = List();
+    for (ComboSante sante in santes) {
+      items.add(
+        DropdownMenuItem(
+          value: sante,
+          child: Text(sante.name),
+        ),
+      );
+    }
+    return items;
+  }
+
+  onChangeDropdownItem(ComboSante selectedsanteDure) {
+    setState(() {
+      _selectedSanteDure = selectedsanteDure;
+    });
+  }
+
   double percent;
   bool valeur = false;
   @override
@@ -74,7 +106,82 @@ class _SanteState extends State<Sante> {
           size: size,
           title: "Créer objectif pour ma sante",
           widget: Column(
-            children: [],
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: size.width * .4,
+                    decoration: BoxDecoration(
+                      color: Grey.withOpacity(.1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: wikiText(
+                        hint: "Montant",
+                        label: "Montant",
+                        inputType: TextInputType.number),
+                  ),
+                  Container(
+                    width: size.width * .4,
+                    decoration: BoxDecoration(
+                      color: Grey.withOpacity(.1),
+                      borderRadius: BorderRadius.circular(5),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: DropdownButton(
+                        style: TextStyle(fontSize: 10.0, color: BlueText),
+                        hint: SingleTitle(
+                          singleTitle: "Selection",
+                          size: 10.0,
+                        ),
+                        value: _selectedSanteDure,
+                        items: _dropdownMenuItems,
+                        onChanged: onChangeDropdownItem,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: 10),
+              Container(
+                width: size.width,
+                decoration: BoxDecoration(
+                  color: Grey.withOpacity(.1),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: MontantSetp(
+                  montant: "150 FCFA",
+                  duree: "${_selectedSanteDure.name}",
+                  value: false,
+                  onTap: () {
+                    setState(() {
+                      //TODO : Update value to bol
+                    });
+                  },
+                ),
+              ),
+              SizedBox(height: 20.0),
+              SizedBox(
+                height: 50,
+                child: WikiButtom(
+                  descpritionButtom: "Suivant",
+                  onPressed: () {},
+                ),
+              ),
+              SizedBox(height: 20.0),
+              SizedBox(
+                height: 50,
+                child: WikiButtom(
+                  descpritionButtom: "Retour",
+                  color: White,
+                  color2: Yello,
+                  colorBorder: Yello,
+                  onPressed: () {},
+                ),
+              ),
+            ],
           ),
         ),
       ),
