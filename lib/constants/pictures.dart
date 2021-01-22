5332d3e29b4afe178c9fb293a9e5abf8a14f119c
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-01-22 02:44:50 -0800

    creation de link for all icon

--- lib/constants/pictures.dart
+++ lib/constants/pictures.dart
@@ -5,4 +5,23 @@ class Pictures {
   static const WelcomImageBackGround = "${pathAssets}logowiqqre.jpg";
   static const ScrollerRight = "${pathAssets}35536-arrow-right.json";
   static const ObjectiSVG = "${pathAssets}35536-arrow-right.json";
+
+  //Icon
+  static const Consltation = "${pathAssets}consulation.png";
+  static const Guide = "${pathAssets}Guide.png";
+  static const Gym = "${pathAssets}gym.png";
+  static const Hopital = "${pathAssets}Hospitalic.png";
+
+  static const Lab2 = "${pathAssets}Lab2.png";
+  static const Lab = "${pathAssets}labo.png";
+  static const Login = "${pathAssets}login.png";
+  static const MAP = "${pathAssets}map.png";
+
+  static const Pharmacie = "${pathAssets}pharmacie.png";
+  static const Prevention = "${pathAssets}Prevention.png";
+  static const Sport = "${pathAssets}sport.png";
+  static const Sportgym = "${pathAssets}Sportgym.png";
+  static const WELCOME = "${pathAssets}welcome.png";
+
+  //
 }
