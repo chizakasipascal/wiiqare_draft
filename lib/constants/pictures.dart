6c736ab942f8af11ffea800ed304c4aa9a2e4223
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-01-25 11:07:03 -0800

    icon home correct

--- lib/constants/pictures.dart
+++ lib/constants/pictures.dart
@@ -7,19 +7,19 @@ class Pictures {
   static const ObjectiSVG = "${pathAssets}35536-arrow-right.json";
 
   //Icon
-  static const Consltation = "${pathAssets}consultation.png";
+  static const Consltation = "${pathAssets}Consultation.png";
   static const Guide = "${pathAssets}Guide.png";
   static const Gym = "${pathAssets}gym.png";
-  static const Hopital = "${pathAssets}Hospitalic.png";
+  static const Hopital = "${pathAssets}HP.png";
 
   static const Lab2 = "${pathAssets}Lab2.png";
-  static const Lab = "${pathAssets}labo.png";
+  static const Lab = "${pathAssets}Lab.png";
   static const Login = "${pathAssets}singin.png";
   static const MAP = "${pathAssets}locationMap.png";
 
-  static const Pharmacie = "${pathAssets}pharmacie.png";
-  static const Prevention = "${pathAssets}Prevention.png";
-  static const Sport = "${pathAssets}sport.png";
+  static const Pharmacie = "${pathAssets}Pharma.png";
+  static const Prevention = "${pathAssets}protect.png";
+  static const Sport = "${pathAssets}fitness Sport.png";
   static const Sportgym = "${pathAssets}Sportgym.png";
   static const WELCOME = "${pathAssets}welcome.png";
 
