30e02338a6be18eb2ebce42bddee046af1acb309
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-01-28 03:50:57 -0800

    correct size

--- lib/views/widgets/Groupe/Steps/GroupeStep2.dart
+++ lib/views/widgets/Groupe/Steps/GroupeStep2.dart
@@ -9,272 +9,274 @@ class GroupeStep2 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      //  mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        SingleTitle(
-          singleTitle:
-              "Comment voulez-vous que l’argent soit utiliser en cas de maladie",
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 10.0),
-        Row(
-          children: [
-            SingleTitle(
-              singleTitle: "1.1 :",
-              color: Grey,
-            ),
-            SizedBox(width: 20.0),
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: White,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 3,
-                      offset: Offset(0, 2), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: SizedBox(
-                  height: 40.0,
-                  child: Center(
-                    child: SingleTitle(
-                      singleTitle:
-                          "Chaque membre consomme uniquement sa contribution",
-                      textAlign: TextAlign.center,
-                      color: Grey,
+    return SingleChildScrollView(
+      child: Column(
+        //  mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SingleTitle(
+            singleTitle:
+                "Comment voulez-vous que l’argent soit utiliser en cas de maladie",
+            textAlign: TextAlign.center,
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              SingleTitle(
+                singleTitle: "1.1 :",
+                color: Grey,
+              ),
+              SizedBox(width: 20.0),
+              Flexible(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: White,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: Center(
+                      child: SingleTitle(
+                        singleTitle:
+                            "Chaque membre consomme uniquement sa contribution",
+                        textAlign: TextAlign.center,
+                        color: Grey,
+                      ),
                     ),
                   ),
                 ),
+              )
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              SingleTitle(
+                singleTitle: "1.2 :",
+                color: Grey,
               ),
-            )
-          ],
-        ),
-        SizedBox(height: 10.0),
-        Row(
-          children: [
-            SingleTitle(
-              singleTitle: "1.2 :",
-              color: Grey,
-            ),
-            SizedBox(width: 20.0),
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: White,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 3,
-                      offset: Offset(0, 2), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: SizedBox(
-                  height: 40.0,
-                  child: Center(
-                    child: SingleTitle(
-                      singleTitle:
-                          "Chaque membre peut uttiliser jusqu’a 2 fois Sa contribution pour sa sante",
-                      textAlign: TextAlign.center,
-                      color: Grey,
+              SizedBox(width: 20.0),
+              Flexible(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: White,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: Center(
+                      child: SingleTitle(
+                        singleTitle:
+                            "Chaque membre peut uttiliser jusqu’a 2 fois Sa contribution pour sa sante",
+                        textAlign: TextAlign.center,
+                        color: Grey,
+                      ),
                     ),
                   ),
                 ),
+              )
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              SingleTitle(
+                singleTitle: "1.3 :",
+                color: Grey,
               ),
-            )
-          ],
-        ),
-        SizedBox(height: 10.0),
-        Row(
-          children: [
-            SingleTitle(
-              singleTitle: "1.3 :",
-              color: Grey,
-            ),
-            SizedBox(width: 20.0),
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: White,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 3,
-                      offset: Offset(0, 2), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: SizedBox(
-                  height: 40.0,
-                  child: Center(
-                    child: SingleTitle(
-                      singleTitle:
-                          "Chaque membre peut uttiliser jusqu’a 3 fois Sa contribution pour sa sante",
-                      textAlign: TextAlign.center,
-                      color: Grey,
+              SizedBox(width: 20.0),
+              Flexible(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: White,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: Center(
+                      child: SingleTitle(
+                        singleTitle:
+                            "Chaque membre peut uttiliser jusqu’a 3 fois Sa contribution pour sa sante",
+                        textAlign: TextAlign.center,
+                        color: Grey,
+                      ),
                     ),
                   ),
                 ),
+              )
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              SingleTitle(
+                singleTitle: "1.4 :",
+                color: Grey,
               ),
-            )
-          ],
-        ),
-        SizedBox(height: 10.0),
-        Row(
-          children: [
-            SingleTitle(
-              singleTitle: "1.4 :",
-              color: Grey,
-            ),
-            SizedBox(width: 20.0),
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: White,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 3,
-                      offset: Offset(0, 2), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: SizedBox(
-                  height: 40.0,
-                  child: Center(
-                    child: SingleTitle(
-                      singleTitle:
-                          "Chaque membre peut uttiliser jusqu’a 4 fois Sa contribution pour sa sante",
-                      textAlign: TextAlign.center,
-                      color: Grey,
+              SizedBox(width: 20.0),
+              Flexible(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: White,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: Center(
+                      child: SingleTitle(
+                        singleTitle:
+                            "Chaque membre peut uttiliser jusqu’a 4 fois Sa contribution pour sa sante",
+                        textAlign: TextAlign.center,
+                        color: Grey,
+                      ),
                     ),
                   ),
                 ),
+              )
+            ],
+          ),
+          SizedBox(height: 10.0),
+          SingleTitle(
+            singleTitle:
+                "Si a la fin de l’annee , vous n’avais pas utiliser l’argent du groupe Wiiqare vous récompense?",
+            fontWeight: FontWeight.bold,
+            textAlign: TextAlign.center,
+            color: Grey,
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              SingleTitle(
+                singleTitle: "25 %",
+                color: Grey,
               ),
-            )
-          ],
-        ),
-        SizedBox(height: 10.0),
-        SingleTitle(
-          singleTitle:
-              "Si a la fin de l’annee , vous n’avais pas utiliser l’argent du groupe Wiiqare vous récompense?",
-          fontWeight: FontWeight.bold,
-          textAlign: TextAlign.center,
-          color: Grey,
-        ),
-        SizedBox(height: 10.0),
-        Row(
-          children: [
-            SingleTitle(
-              singleTitle: "25 %",
-              color: Grey,
-            ),
-            SizedBox(width: 20.0),
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: White,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 3,
-                      offset: Offset(0, 2), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: SizedBox(
-                  height: 40.0,
-                  child: Center(
-                    child: SingleTitle(
-                      singleTitle: "25% de l’argent du groupe est rembourse",
-                      textAlign: TextAlign.center,
-                      color: Grey,
+              SizedBox(width: 20.0),
+              Flexible(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: White,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: Center(
+                      child: SingleTitle(
+                        singleTitle: "25% de l’argent du groupe est rembourse",
+                        textAlign: TextAlign.center,
+                        color: Grey,
+                      ),
                     ),
                   ),
                 ),
+              )
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              SingleTitle(
+                singleTitle: "50 %",
+                color: Grey,
               ),
-            )
-          ],
-        ),
-        SizedBox(height: 10.0),
-        Row(
-          children: [
-            SingleTitle(
-              singleTitle: "50 %",
-              color: Grey,
-            ),
-            SizedBox(width: 20.0),
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: White,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 3,
-                      offset: Offset(0, 2), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: SizedBox(
-                  height: 40.0,
-                  child: Center(
-                    child: SingleTitle(
-                      singleTitle: "50% de l’argent du groupe est rembourse",
-                      textAlign: TextAlign.center,
-                      color: Grey,
+              SizedBox(width: 20.0),
+              Flexible(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: White,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: Center(
+                      child: SingleTitle(
+                        singleTitle: "50% de l’argent du groupe est rembourse",
+                        textAlign: TextAlign.center,
+                        color: Grey,
+                      ),
                     ),
                   ),
                 ),
+              )
+            ],
+          ),
+          SizedBox(height: 10.0),
+          Row(
+            children: [
+              SingleTitle(
+                singleTitle: "75 %",
+                color: Grey,
               ),
-            )
-          ],
-        ),
-        SizedBox(height: 10.0),
-        Row(
-          children: [
-            SingleTitle(
-              singleTitle: "75 %",
-              color: Grey,
-            ),
-            SizedBox(width: 20.0),
-            Flexible(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: White,
-                  boxShadow: [
-                    BoxShadow(
-                      color: Grey.withOpacity(0.5),
-                      spreadRadius: 2,
-                      blurRadius: 3,
-                      offset: Offset(0, 2), // changes position of shadow
-                    ),
-                  ],
-                ),
-                child: SizedBox(
-                  height: 40.0,
-                  child: Center(
-                    child: SingleTitle(
-                      singleTitle: "75% de l’argent du groupe est rembourse",
-                      textAlign: TextAlign.center,
-                      color: Grey,
+              SizedBox(width: 20.0),
+              Flexible(
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: White,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Grey.withOpacity(0.5),
+                        spreadRadius: 2,
+                        blurRadius: 3,
+                        offset: Offset(0, 2), // changes position of shadow
+                      ),
+                    ],
+                  ),
+                  child: SizedBox(
+                    height: 40.0,
+                    child: Center(
+                      child: SingleTitle(
+                        singleTitle: "75% de l’argent du groupe est rembourse",
+                        textAlign: TextAlign.center,
+                        color: Grey,
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
-          ],
-        ),
-      ],
+              )
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/views/widgets/Groupe/Steps/groupStepOne.dart
+++ lib/views/widgets/Groupe/Steps/groupStepOne.dart
@@ -9,94 +9,96 @@ class GroupeStep1 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        SingleTitle(
-          singleTitle:
-              "Créer un groupe santé est une bonne manier de se protege en cas de problème de santé grave",
-          textAlign: TextAlign.center,
-        ),
-        SizedBox(height: 10),
-        SingleTitle(
-          singleTitle: "Nom du groupe sante:",
-          color: Grey,
-        ),
-        SizedBox(height: 5.0),
-        Container(
-          decoration: BoxDecoration(
-            color: White,
-            boxShadow: [
-              BoxShadow(
-                color: Grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 3,
-                offset: Offset(0, 2), // changes position of shadow
-              ),
-            ],
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SingleTitle(
+            singleTitle:
+                "Créer un groupe santé est une bonne manier de se protege en cas de problème de santé grave",
+            textAlign: TextAlign.center,
           ),
-          child: wikiText(hint: "Ex WiiQare"),
-        ),
-        SizedBox(height: 5.0),
-        SingleTitle(
-          singleTitle: "Description du groupe sante",
-          color: Grey,
-        ),
-        SizedBox(height: 5.0),
-        Container(
-          decoration: BoxDecoration(
-            color: White,
-            boxShadow: [
-              BoxShadow(
-                color: Grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 3,
-                offset: Offset(0, 2), // changes position of shadow
-              ),
-            ],
+          SizedBox(height: 10),
+          SingleTitle(
+            singleTitle: "Nom du groupe sante:",
+            color: Grey,
           ),
-          child: wikiText(hint: "Combo box"),
-        ),
-        SingleTitle(
-          singleTitle: "Combien de chaque doit continue",
-          color: Grey,
-        ),
-        SizedBox(height: 5.0),
-        Container(
-          decoration: BoxDecoration(
-            color: White,
-            boxShadow: [
-              BoxShadow(
-                color: Grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 3,
-                offset: Offset(0, 2), // changes position of shadow
-              ),
-            ],
+          SizedBox(height: 5.0),
+          Container(
+            decoration: BoxDecoration(
+              color: White,
+              boxShadow: [
+                BoxShadow(
+                  color: Grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 3,
+                  offset: Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: wikiText(hint: "Ex WiiQare"),
           ),
-          child: wikiText(hint: "Combo box"),
-        ),
-        SingleTitle(
-          singleTitle: "Choisis la fréquence pour contribue",
-          color: Grey,
-        ),
-        SizedBox(height: 5.0),
-        Container(
-          decoration: BoxDecoration(
-            color: White,
-            boxShadow: [
-              BoxShadow(
-                color: Grey.withOpacity(0.5),
-                spreadRadius: 2,
-                blurRadius: 3,
-                offset: Offset(0, 2), // changes position of shadow
-              ),
-            ],
+          SizedBox(height: 5.0),
+          SingleTitle(
+            singleTitle: "Description du groupe sante",
+            color: Grey,
           ),
-          child: wikiText(hint: "Combo box"),
-        ),
-      ],
+          SizedBox(height: 5.0),
+          Container(
+            decoration: BoxDecoration(
+              color: White,
+              boxShadow: [
+                BoxShadow(
+                  color: Grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 3,
+                  offset: Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: wikiText(hint: "Combo box"),
+          ),
+          SingleTitle(
+            singleTitle: "Combien de chaque doit continue",
+            color: Grey,
+          ),
+          SizedBox(height: 5.0),
+          Container(
+            decoration: BoxDecoration(
+              color: White,
+              boxShadow: [
+                BoxShadow(
+                  color: Grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 3,
+                  offset: Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: wikiText(hint: "Combo box"),
+          ),
+          SingleTitle(
+            singleTitle: "Choisis la fréquence pour contribue",
+            color: Grey,
+          ),
+          SizedBox(height: 5.0),
+          Container(
+            decoration: BoxDecoration(
+              color: White,
+              boxShadow: [
+                BoxShadow(
+                  color: Grey.withOpacity(0.5),
+                  spreadRadius: 2,
+                  blurRadius: 3,
+                  offset: Offset(0, 2), // changes position of shadow
+                ),
+              ],
+            ),
+            child: wikiText(hint: "Combo box"),
+          ),
+        ],
+      ),
     );
   }
 }
