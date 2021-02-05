fada758ad43348ea481cca277c5030e4d7388a5c
pascal kasi chiza <pascalkasichiza@gmail.com>
2021-02-05 12:08:47 -0800

    hopital ui ok

--- lib/views/tabsRecompense/offres.dart
+++ lib/views/tabsRecompense/offres.dart
@@ -1,7 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:wiiqare/constants/routes.dart';
-import 'package:wiiqare/utils/colors.dart';
-import 'package:wiiqare/views/widgets/widgets.dart';
 import 'package:wiiqare/views/widgets/wikiOffres.dart';
 
 class Offres extends StatefulWidget {
--- lib/views/ui/labo.dart
+++ lib/views/ui/labo.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
+import 'package:wiiqare/views/widgets/wikiOffres.dart';
 
 class Labo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Container(
-      child: Text("Labo"),
-    )));
+    final size = MediaQuery.of(context).size;
+    return ListView.builder(
+      //  controller: scrollController,
+      itemCount: 60,
+      physics: BouncingScrollPhysics(),
+      itemBuilder: (context, i) => new Container(
+        // height: 300.0,
+        //  margin: const EdgeInsets.only(top: 9),
+        // color: Colors.greenAccent,
+
+        child: ClipRRect(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          child: WikiOffres(
+            size: MediaQuery.of(context).size,
+            bonus: 1000,
+            onPressed: () {
+              //TODO: Code propres aceete fonction
+            },
+          ),
+        ),
+      ),
+    );
   }
 }
